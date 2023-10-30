92bd6f1b671a91629bfa8bf95ed0dce9dbdf7a0b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-30 15:39:08 +0100

    [ add ] addtask function wirth form data

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ const {createApp}= Vue;
 createApp({
   data(){
     return{
+      dataUrl:'server.php',
       title:'Todo List',
-      list:[]
+      list:[],
+      newTask:''
     }
   },
   methods:{
@@ -14,6 +16,18 @@ createApp({
         console.log(result.data);
         this.list = result.data;
       })
+    },
+    addTask(){
+      const data = new FormData();
+      data.append('todoItem',this.newTask);
+
+      axios.post(this.dataUrl,data)
+      .then(result =>{
+        this.list = result.data;
+        this.newTask = ''
+        console.log('ADD');
+      })
+
     }
   },
   mounted(){
