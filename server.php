8a420a53286bc350bcb3c2715726fcd2dcfd1c50
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-30 16:47:28 +0100

    [ add ] comments

--- server.php
+++ server.php
@@ -4,12 +4,14 @@ $json_string = file_get_contents('todo-list.json');
 
 $task = json_decode($json_string);
 
+// ADD
 if(isset($_POST['todoItem']) && !empty($_POST['todoItem'])){
   $newTask = $_POST['todoItem'];
   $task[] = $newTask;
   file_put_contents('todo-list.json', json_encode($task,true));
 }
 
+// DELETE 
 if(isset($_POST['delIndex'])){
   $delTask = $_POST['delIndex'];
   array_splice($task,$delTask,1);
@@ -23,3 +25,4 @@ $taskString = json_encode($task,true);
 
 echo $taskString;
 
+// [{"text":"Allenamento","doneTask":"false"},{"text":"Aggiornare status Linkedin","doneTask":"false"},{"text":"Comprare crocchette","doneTask":"false"}]
