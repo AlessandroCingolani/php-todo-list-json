2963ca9a030161abee5e3f9195664cffdb1ecd16
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-30 14:54:39 +0100

    [ init ] vue with get axios server tasks

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ const {createApp}= Vue;
 createApp({
   data(){
     return{
-
+      title:'Todo List'
     }
   },
+  methods:{
+    getTask(){
+      axios.get('server.php')
+      .then(result =>{
+        console.log(result.data);
+      })
+    }
+  },
+  mounted(){
+    this.getTask();
+  }
 
   
 
