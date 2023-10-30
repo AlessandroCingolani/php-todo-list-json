2a30a4fedbd7b5431d41670d5b453ee47cc8cc86
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-30 15:55:07 +0100

    [ add ] if for empty task

--- server.php
+++ server.php
@@ -4,7 +4,7 @@ $json_string = file_get_contents('todo-list.json');
 
 $task = json_decode($json_string);
 
-if(isset($_POST['todoItem'])){
+if(isset($_POST['todoItem']) && !empty($_POST['todoItem'])){
   $newTask = $_POST['todoItem'];
   $task[] = $newTask;
   file_put_contents('todo-list.json', json_encode($task,true));
